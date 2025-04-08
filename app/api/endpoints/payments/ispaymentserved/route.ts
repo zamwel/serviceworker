5cabcfdb89a8c111d0f9f08c6018f11e9b1632a2
zamwel <otengs16@gmail.com>
2025-04-08 23:31:01 +0000

    paymentserved

--- app/api/endpoints/payments/ispaymentserved/route.ts
+++ app/api/endpoints/payments/ispaymentserved/route.ts
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 
 
 export async function POST(req: Request) {
-    const { paymentid } = await req.json();
+    const { paymentId } = await req.json();
     try {
-        const data = await isPaymentServed(paymentid);
+        const data = await isPaymentServed(paymentId);
         return NextResponse.json( data , { status: 201 });
     } catch (error: any) {
          return NextResponse.json({message:error.message}, { status: 400 });
